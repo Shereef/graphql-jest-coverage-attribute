3cbb0cb22a85b708003d1ac3e8987fb8ae05840f
Shereef Marzouk <shereef.marzouk@daysmart.com>
2022-11-18 16:21:15 -0400

    Added graphql to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: [null, 'local', 'dev'].some(
+        (env) => process.env.NODE_ENV == env,
+      ),
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
